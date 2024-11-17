f704085bd5243a5d348d6fc4cecdd8badda48feb
Christian2473 <135941125+Christian2473@users.noreply.github.com>
2024-11-16 20:08:19 -0500

    Dropping 12M treasuries and fixed stuff in DateRanges.py

--- DateRanges.py
+++ DateRanges.py
@@ -1,11 +1,15 @@
-from ETFTestingData import election_day
 import datetime as dt
+import pandas as pd
+
+def election_day(year):
+    date = pd.Timestamp(year=year, month=11, day=1)
+    return date + pd.offsets.Week(weekday=1)
 
 class ElectionYears:
     """Getting information for date ranges during election years"""
     def __init__(self):
         self.start_date = dt.datetime(year = 1992, month = 1, day = 1)
-        self.o_range = 2024 - self.start_date #overall range
+        self.o_range = 2024 - self.start_date.year #overall range
         self.num_ranges = self.o_range // 4
 
     def ranges(self):
@@ -19,23 +23,22 @@ class ElectionYears:
         # values of r_ranges will be lists. index 1 returns the start date. Index 
 
         for i in range(self.num_ranges):
-            year = self.start_date + 4*i
-            e_day = dt.datetime(election_day(year))
+            year = self.start_date.year + 4*i
+            e_day = election_day(year).to_pydatetime()
             start = dt.datetime(year = year, month = e_day.month - 6, day = e_day.day)
             end = e_day + dt.timedelta(days = 14)
 
             r_ranges[str(year)] = [start, end]
 
-            return r_ranges
+        return r_ranges
 
 class NonElectionYears(ElectionYears):
     """Getting information for date ranges during non-election years"""
     def __init__(self, years_after_election =1):
         if years_after_election not in [1,3]:
             raise ValueError("Input must be either 1 or 3")
-        print("You entered" + str(years_after_election))
-        super().__init__()
-        self.start_date = dt.datetime(year = 1992, month = 1, day = 1)
-        self.o_range = 2024 - self.start_date #overall range
+            print("You entered" + str(years_after_election))
+        self.start_date = dt.datetime(year = 1992 + years_after_election, month = 1, day = 1)
+        self.o_range = 2024 - self.start_date.year #overall range
         self.num_ranges = self.o_range // 4
 
